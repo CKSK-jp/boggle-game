de9a72f1580826c3ad4851ba4790881a63c9b589
cksk-jp <carlson.ksk@gmail.com>
2024-01-15 13:07:11 -0500

    file upload

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+from flask import Flask, render_template, request
+
 from boggle import Boggle
 
 boggle_game = Boggle()
+app = Flask(__name__)
+
+
+@app.route("/")
+def story_selection():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 from unittest import TestCase
-from app import app
+
 from flask import session
+
+from app import app
 from boggle import Boggle
 
 
 class FlaskTests(TestCase):
-
     # TODO -- write tests for every view function / feature!
 
+    def display_board(self):
+        with app.test_client() as client:
+            # import pdb
+            # pdb.set_trace
+            res = client.get("/")
+            html = res.get_data(as_text=True)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn("<h1>Test Title</h1>", html)
