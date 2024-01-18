3164852528d2a9fa40fc11b3d8f3a4188030595e
cksk-jp <carlson.ksk@gmail.com>
2024-01-18 08:36:49 -0500

    README, docstrings, styling

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # boggle-game
 The goal of the game is to get the highest point total. To gain points, players create words from a random assortment of letters in a 5x5 grid.
 
+## **Set Up **
+
+```bash
+$python3 -m venv venv
+```
+
+```bash
+$source venv/bin/activate
+(env) $
+
+```
+
+```bash
+(env) $pip3 install flask
+...
+
+```
+
+Make a “requirements.txt” file in this directory with a listing of all the software needed for this project:
+```
+(env) $pip3 freeze > requirements.txt
+```
+
+### **Usage Instructions **
+- User can find words by chaining words in the horizontal, vertical, or diagonal axis.
+- Entering a valid word will award a score proportional to the length of the word
+- Selecting a timed game will set a 60 second timer
+- Scores are saved server side using session
+- Timed Games will be saved in both localStorage and server side sessions
+
+**Technologies Used:** HTML, CSS, JavaScript, Flask, Axios, Python, Unittest
+
 ## TODO 
 Implement timer
--- app.py
+++ app.py
@@ -6,6 +6,13 @@ from boggle import Boggle
 
 
 class BoggleApp(Flask):
+    """
+    Initialize the BoggleApp.
+
+    Parameters:
+    - import_name (str): The name of the import package.
+    """
+
     def __init__(self, import_name):
         super().__init__(import_name)
         self.config["SECRET_KEY"] = "asdf123"
@@ -16,10 +23,14 @@ class BoggleApp(Flask):
 
 app = BoggleApp(__name__)
 
+# set the time limit for a timed game
+time_limit_seconds = 60
+
 
 @app.route("/")
 def boggle_homepage():
-    session.setdefault("current_board", [])
+    """Render the homepage for the Boggle game."""
+    session.setdefault("current_board")
     session.setdefault("found_words", [])
     session.setdefault("current_score", 0)
     session.setdefault("highscore", 0)
@@ -35,6 +46,12 @@ def boggle_homepage():
 
 @app.route("/submit-guess", methods=["POST"])
 def submit_guess():
+    """
+    Handles the submission of a word guess.
+
+    Returns:
+    - JSON response containing information about the guess result.
+    """
     data = request.get_json()
     guess = data.get("guess")
     result = app.boggle_instance.check_valid_word(session["current_board"], guess)
@@ -43,9 +60,7 @@ def submit_guess():
         if guess not in session["found_words"]:
             message = "you found a word!"
             session["found_words"].append(guess)
-            session["current_score"] += len(guess)
-            if session["current_score"] > session["highscore"]:
-                session["highscore"] = session["current_score"]
+            check_score(guess)
         else:
             message = "already guessed!"
 
@@ -53,7 +68,6 @@ def submit_guess():
         message = "invalid guess"
     else:
         message = "not a word"
-    print(session["found_words"], session["current_score"])
     return jsonify(
         {
             "foundWords": session["found_words"],
@@ -64,8 +78,25 @@ def submit_guess():
     )
 
 
+def check_score(guess):
+    """
+    Update the game score based on the submitted word guess.
+
+    Parameters:
+    - guess (str): The word guessed by the player.
+
+    Returns:
+    - Tuple of the current score and high score.
+    """
+    session["current_score"] += len(guess)
+    if session["current_score"] > session["highscore"]:
+        session["highscore"] = session["current_score"]
+    return session["current_score"], session["highscore"]
+
+
 @app.route("/reset-game")
 def reset_game():
+    """Handles game reset press, ensuring necessary session variables are reset."""
     session["found_words"] = []
     session["current_board"] = app.boggle_instance.make_board()
     session["current_score"] = 0
@@ -76,9 +107,14 @@ def reset_game():
 
 @app.route("/start_timer")
 def start_timer():
-    # start a new duration if not currently in a timedGame
+    """
+    Start the timer for the timed game.
+
+    Returns:
+    - JSON response containing the remaining time.
+    """
     if "duration" not in app.config:
-        app.config["duration"] = datetime.now() + timedelta(seconds=60)
+        app.config["duration"] = datetime.now() + timedelta(seconds=time_limit_seconds)
     remaining_time = app.config["duration"] - datetime.now()
     remaining_seconds = remaining_time.total_seconds()
 
@@ -91,8 +127,9 @@ def start_timer():
 
 @app.route("/stop_timer", methods=["POST"])
 def stop_timer():
+    """Stop the timer for the timed game."""
     app.config.pop("duration", None)
-    return ""
+    return {"message": "Timer stopped successfully."}
 
 
 if __name__ == "__main__":
